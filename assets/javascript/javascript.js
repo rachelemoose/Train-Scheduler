de0afbf45e0aa6fb3d7e2aa2a2d3def6b98f060a
R.E. Moose <rachelemoose@gmail.com>
2018-11-10 10:49:10 -0800

    final draft

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -27,21 +27,23 @@
 
 
 //create object for holding new train data 
+//first fields before the colon are Firebase fields created
+//second fields after the colon are variables created in this file
   var newTrain = {
-    name: addedTrain,
-    destin: destination,
-    firstTT: firstTrainTime,
-    frequence: frequency,
+    trainName: addedTrain,
+    trainDestination: destination,
+    theFirstTrainTime: firstTrainTime,
+    frequencyofTrain: frequency,
 }
 
   //upload new train data to firebase
   database.ref().push(newTrain);
 
   //log it all to make sure it's correctly capturing
-  console.log(newTrain.name);
-  console.log(newTrain.destin);
-  console.log(newTrain.firstTT);
-  console.log(newTrain.frequence)
+  console.log("New Train stored in Firebase: " + newTrain.trainName);
+  console.log("New Train's destination: " + newTrain.trainDestination);
+  console.log("What Time the First Train Arrives: " + newTrain.theFirstTrainTime);
+  console.log("How Often the New Train Runs: " + newTrain.frequencyofTrain)
 
 //clear the values after submitting 
   $("#train_name").val("");
@@ -49,24 +51,39 @@
   $("#first_train_time").val("");
   $("#frequency").val("");
 
+//create variable and log current time to make sure it is working correctly
+  var currentTime = moment();
+  console.log(currentTime) //this does not update unless refreshed 
+
+// Difference between the current time and the next train time
+    var diffTime = moment().diff(moment(currentTime), "minutes");
+
+// Time apart (remainder)
+  var remaining = diffTime % frequency;
+    
+
+// var MinutesTillTrain = frequency - remainder;
+  var MinutesTillTrain = frequency - remaining;
+
+  var nextTrain = moment().add(MinutesTillTrain, "minutes").format("hh:mm");
+  console.log("Next train: " + nextTrain)
+
   });
 
 
 database.ref().on("child_added", function(childSnaphot) {
     console.log(childSnaphot.val());
 
-    var addedTrain = childSnaphot.val().name;
+    var addedTrain = childSnaphot.val().trainName;
     console.log(addedTrain)
-    var destination = childSnaphot.val().destin;
+    var destination = childSnaphot.val().trainDestination;
     console.log(destination)
-    var firstTrainTime = childSnaphot.val().firstTT;
+    var firstTrainTime = childSnaphot.val().theFirstTrainTime;
     console.log(firstTrainTime)
-    var frequency = childSnaphot.val().frequence;
+    var frequency = childSnaphot.val().frequencyofTrain;
     console.log(frequency)
 
-    //create variable and log current time to make sure it is working correctly
-  var currentTime = moment().format("DD/MM/YY hh:mm A");
-  console.log(currentTime) //this does not update unless refreshed 
+
 
   function displayRealTime() {
     setInterval(function(){
@@ -75,8 +92,33 @@ database.ref().on("child_added", function(childSnaphot) {
   }
  displayRealTime();
 
+  //create variable and log current time to make sure it is working correctly
+   var currentTime = moment();
+   console.log("current time with moment" + currentTime) //this does not update unless refreshed 
+
+	//take first train time and subtract a year - gives me a long string of numbers
+	var firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+	console.log("First train time converted back a year: " + firstTimeConverted)
+
+// Difference between the current time and the next train time
+ var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+ console.log("Difference in time between now and next train: " + diffTime);
+
+// Time apart
+  var remaining = diffTime % frequency;
+  console.log("remaining time: " + remaining);
+
+  // var MinutesTillTrain = frequency - remainder;
+  var MinutesTillNextTrain = frequency - remaining;
+  console.log("Minutes until train: " + MinutesTillNextTrain);
+
+
+ var nextTrain = moment().add(MinutesTillNextTrain, "minutes").format("hh:mm");
+ console.log("next train arrival time: " + moment(nextTrain).format("hh:mm"));
+
+
  $("#trainBody").append("<tr><td>" + addedTrain + "</td><td>" + destination + "</td><td>" + frequency +
- "</td>" + firstTrainTime + "</td></tr>")
+ "</td>" + firstTrainTime + "</td><td>" + nextTrain + "</td><td>" + MinutesTillNextTrain + "</td></tr>")
 
 })
 
